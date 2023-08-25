ff7d8ec6ad27aeabf3f41448c61f6f73c5da68ca
kenneth adutwum <kwamayadu@gmail.com>
2023-08-25 17:09:17 +0000

    Fix some functionalities in the script file

--- src/index.js
+++ src/index.js
@@ -1,45 +1,50 @@
 import './styles/style.css';
-import { populateTasks, addTask, editTask, clearCompletedTasks, removeTask } from './modules/operationFunctions';
-
-// const tasks = [
-//   { description: 'Go to church', completed: false, index: 1 },
-//   { description: 'Time with friend', completed: true, index: 2 },
-//   { description: 'Chores', completed: false, index: 3 },
-// ];
-
+import {
+  populateTasks,
+  addTask,
+  editTask,
+  clearCompletedTasks,
+  removeTask,
+} from './modules/operationFunctions.js';
 
 // Initialize tasks from local storage
 populateTasks();
 
 // Add task functionality
 document.getElementById('add-btn').addEventListener('click', () => {
-  const newTaskDescription = document.getElementById('newList').value;
-  if (newTaskDescription.trim() !== '') { 
-    addTask(newTaskDescription);
+  const newTaskDescription = document.getElementById('newList');
+  if (newTaskDescription.value.trim() !== '') {
+    addTask(newTaskDescription.value);
+    newTaskDescription.value = '';
   }
 });
 
 // Delete task functionality
 document.getElementById('list').addEventListener('click', (event) => {
   if (event.target.classList.contains('fa-trash')) {
-    const taskIndex = event.target.getAttribute('data-index');
+    const taskList = event.target.parentElement.childNodes[0];
+    const taskItem = taskList.querySelector('.item');
+    const taskIndex = taskItem.getAttribute('data-index');
     removeTask(taskIndex);
-  } else if(event.target.classList.contains('fa-ellipsis-v')) {
+  } else if (event.target.classList.contains('fa-ellipsis-v')) {
     event.target.className = 'fas fa-trash trash';
   }
 });
 
+function handleFocus(event) {
+  const inputValue = event.target.value;
+  const taskIndex = event.target.getAttribute('data-index');
+  const newDescription = inputValue.trim();
+  editTask(taskIndex, newDescription);
+}
+
 // Edit task functionality
-document.getElementById('list').addEventListener('input', (event) => {
-  if (event.target.classList.contains('list-item')) {
-    const taskIndex = event.target.getAttribute('data-index');
-    const newDescription = event.target.textContent.trim();
-    editTask(taskIndex, newDescription);
-  }
+const element = document.querySelectorAll('#editForm');
+element.forEach((el) => {
+  el.addEventListener('focusout', handleFocus);
 });
 
 // Clear completed tasks
 document.getElementById('clear').addEventListener('click', () => {
   clearCompletedTasks();
 });
-
--- src/modules/operationFunctions.js
+++ src/modules/operationFunctions.js
@@ -1,42 +1,47 @@
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+const inverseTasks = tasks.reverse();
 
 const saveTasksToLocalStorage = () => {
   localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+};
 
 const populateTasks = () => {
-    const ulTag = document.getElementById('list');
-  
-    while (ulTag.hasChildNodes()){
-      ulTag.removeChild(ulTag.firstChild)
-    }
+  const ulTag = document.getElementById('list');
+  while (ulTag.hasChildNodes()) {
+    ulTag.removeChild(ulTag.firstChild);
+  }
 
-    tasks.forEach((task) => {
-      //<li>
-      const listItem = document.createElement('li');
-      listItem.className = 'list-item';
-      listItem.innerHTML = `
+  inverseTasks.forEach((task) => {
+    // <li>
+
+    const listItem = document.createElement('li');
+
+    listItem.className = 'list-item';
+    listItem.innerHTML = `
         <input type="checkbox" ${task.completed ? 'checked' : ''}>
-        ${task.description}
+        <input type='text' id='editForm' class='item' value = ${
+  task.description
+} data-index='${task.index}'>
       `;
 
+    // <i>
+    const iTag = document.createElement('i');
+    iTag.className = 'fas fa-ellipsis-v';
 
-      //<i>
-      const iTag = document.createElement('i');
-      iTag.className = "fas fa-ellipsis-v";
+    // <div>
+    const divTag = document.createElement('div');
+    ulTag.appendChild(divTag);
 
+    divTag.className = 'list-container';
+    divTag.appendChild(listItem);
+    divTag.appendChild(iTag);
+  });
+};
 
-      // <div>
-      const divTag = document.createElement('div');
-      ulTag.appendChild(divTag);
-
-      divTag.className = 'list-container';
-      divTag.appendChild(listItem);
-      divTag.appendChild(iTag);
-     
-      
-    });
-  }
+function clearForm() {
+  // Replace 'myForm' with the actual ID of your form element
+  document.getElementById('newList');
+}
 
 const addTask = (description) => {
   const newTask = {
@@ -47,25 +52,34 @@ const addTask = (description) => {
   tasks.push(newTask);
   saveTasksToLocalStorage();
   populateTasks();
-}
+  clearForm();
+};
 
 const removeTask = (index) => {
-  tasks.splice(index - 1, 1);
-  tasks.forEach((task, idx) => (task.index = idx + 1));
+  tasks.splice(index, 1);
+  tasks.forEach((task, idx) => {
+    task.index = idx;
+  });
   saveTasksToLocalStorage();
   populateTasks();
-}
+};
 
 const editTask = (index, newDescription) => {
   tasks[index - 1].description = newDescription;
   saveTasksToLocalStorage();
-}
+};
 
 const clearCompletedTasks = () => {
-  tasks = tasks.filter(task => !task.completed);
-  tasks.forEach((task, idx) => (task.index = idx + 1));
+  tasks = tasks.filter((task) => !task.completed);
+
+  tasks.forEach((task, idx) => {
+    task.index = idx + 1;
+  });
+
   saveTasksToLocalStorage();
   populateTasks();
-}
+};
 
-export { populateTasks, addTask, removeTask, editTask, clearCompletedTasks };
+export {
+  populateTasks, addTask, removeTask, editTask, clearCompletedTasks,
+};
